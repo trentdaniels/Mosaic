ab158ce4833a4cd06af899188044c307651d8a37
Trenton Daniels <trentondaniels95@gmail.com>
2018-10-03 16:11:39 -0500

    cleaning up select control on main search component

--- src/components/MainSearch.vue
+++ src/components/MainSearch.vue
@@ -1,25 +1,18 @@
 <template>
   <div id="mainSearch">
     <label class="label has-text-white">Start Searching:</label>
-    <div  class="field has-addons" @keyup.enter="getProjects">
-    <div class="control">
-      <div class="select is-medium is-primary">
-        <template v-if="user">
-          <select v-model="selectedApi">
+    <b-field @keyup.enter="getProjects" type="is-primary">
+        <b-select placeholder="Purpose" icon="earth" v-model="selectedApi" v-if="user" size="is-medium">
             <option v-for="(api, index) in apis" :key="index" :value="index">
               {{ api }}
             </option>
-          </select>
-        </template>
-        <template v-else>
-          <select v-model="selectedApi">
+        </b-select>
+        <b-select placeholder="Purpose" icon="earth" v-model="selectedApi" v-else size="is-medium">
             <option v-for="(api, index) in apis" :key="index" :value="index" v-if="index > 0">
               {{ api }}
             </option>
-          </select>
-        </template>
-      </div>
-    </div>
+        </b-select>
+   
     <template v-if="user">
       <div class="control is-expanded">
         <input type="text" placeholder="ex: Rainbow Dragons" v-model="search" class="input is-medium is-primary" v-if="selectedApi !== 0" />
@@ -38,7 +31,7 @@
     <div class="control">
       <button class="button is-primary is-medium" :class="{'is-loading': loading}" @click="getProjects">Search</button>
     </div>
-  </div>
+  </b-field>
   </div>
 </template>
 
